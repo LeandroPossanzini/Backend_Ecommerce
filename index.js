596930e5d5717be60ae7adedb6c949535a076c4f
Leandro Possanzini <leandropossanzini@gmail.com>
2022-08-03 12:10:16 -0300

    WebSocket en la base de datos

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const loginUser = require("./auth/login")
 const products = require("./routes/products")
 const carts = require("./routes/carts")
 const orders = require("./routes/order")
+const Message = require("./models/Message")
+
+const { Server: HttpServer } = require('http')
+const { Server: IOServer } = require('socket.io')
+
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
+
 
 dotenv.config();
 app.use(express.json())
@@ -16,8 +24,28 @@ mongoose
     .then(() => console.log("*****Base de datos conectada*****"))
     .catch((e) => console.log(e))
 
+// WEBSOCKET
+const mensajes = []
+io.on('connection', socket => {
 
-    
+    socket.emit('mensajes', mensajes)
+    socket.on('nuevoMensaje', mensaje => {
+        mensajes.push(mensaje)
+        io.sockets.emit('mensajes', mensajes)
+        const guardar = async () =>{
+            const newMessage = new Message(mensaje)
+            try {
+                const guardar = await newMessage.save()
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        guardar()
+    })
+})
+
+
+app.use(express.static('public'))     
 app.use("/registro" , registerUser)
 app.use("/login" , loginUser)
 app.use("/login", userRoute )    
@@ -25,6 +53,6 @@ app.use("/api/products", products)
 app.use("/api/shoppingcartproducts", carts)
 app.use("/api/orders", orders)
 
-app.listen(process.env.PORT || 5000, () => {
+httpServer.listen(process.env.PORT || 5000, () => {
     console.log("Servidor ok")
 })
